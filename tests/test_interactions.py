b2ab851511b8d232f0d9d792f05296d017fb2f03
Cédric Bouysset <cedric.bouysset@unice.fr>
2020-12-04 20:11:26 +0100

    refactoring interactions tests

--- tests/test_interactions.py
+++ tests/test_interactions.py
@@ -2,120 +2,147 @@ import pytest
 import os
 from rdkit import Chem, RDLogger
 import numpy as np
-import MDAnalysis as mda
+from MDAnalysis import Universe
+from MDAnalysis.topology.guessers import guess_atom_element
 from prolif.molecule import Molecule
 from prolif.fingerprint import Fingerprint
-
+from prolif.datafiles import datapath
 
 # disable rdkit warnings
 lg = RDLogger.logger()
 lg.setLevel(RDLogger.ERROR)
 
 
-def from_mol2(f):
-    path = str(prolif.datafiles.datapath / f)
-    u = mda.Universe(path)
-    elements = [mda.topology.guessers.guess_atom_element(n)
-                for n in u.atoms.names]
-    u.add_TopologyAttr("elements", np.array(elements, dtype=object))
-    return prolif.Molecule.from_mda(u)
+class MolFactory:
+    def from_mol2(self, f):
+        path = str(datapath / f)
+        u = Universe(path)
+        elements = [guess_atom_element(n) for n in u.atoms.names]
+        u.add_TopologyAttr("elements", np.array(elements, dtype=object))
+        return Molecule.from_mda(u)
 
-
-@pytest.mark.skip(reason="Needs refactoring (open issue mol2 needs bonds...")
-class TestInteractions:
-    @pytest.fixture
     def benzene(self):
-        return from_mol2("benzene.mol2")
-    
-    @pytest.fixture
+        return self.from_mol2("benzene.mol2")
+
     def cation(self):
-        return from_mol2("cation.mol2")
+        return self.from_mol2("cation.mol2")
 
-    @pytest.fixture
+    def cation_false(self):
+        return self.from_mol2("cation_false.mol2")
+
     def anion(self):
-        return from_mol2("anion.mol2")
+        return self.from_mol2("anion.mol2")
 
-    @pytest.fixture
     def ftf(self):
-        return from_mol2("facetoface.mol2")
+        return self.from_mol2("facetoface.mol2")
 
-    @pytest.fixture
     def etf(self):
-        return from_mol2("edgetoface.mol2")
-    
+        return self.from_mol2("edgetoface.mol2")
+
+    def chlorine(self):
+        return self.from_mol2("chlorine.mol2")
+
+    def hb_donor(self):
+        return self.from_mol2("donor.mol2")
+
+    def hb_acceptor(self):
+        return self.from_mol2("acceptor.mol2")
+
+    def hb_acceptor_false(self):
+        return self.from_mol2("acceptor_false.mol2")
+
+    def xb_donor(self):
+        return self.from_mol2("xbond_donor.mol2")
+
+    def xb_acceptor(self):
+        return self.from_mol2("xbond_acceptor.mol2")
+
+    def xb_acceptor_false_xar(self):
+        return self.from_mol2("xbond_acceptor_false_xar.mol2")
+
+    def xb_acceptor_false_axd(self):
+        return self.from_mol2("xbond_acceptor_false_axd.mol2")
+
+    def ligand(self):
+        return self.from_mol2("ligand.mol2")
+
+    def metal(self):
+        return self.from_mol2("metal.mol2")
+
+    def metal_false(self):
+        return self.from_mol2("metal_false.mol2")
+
+
+@pytest.fixture(scope="module")
+def mol1(request):
+    return getattr(MolFactory, request.param)()
+
+
+@pytest.fixture(scope="module")
+def mol2(request):
+    return getattr(MolFactory, request.param)()
+
+
+class TestInteractions:
     @pytest.fixture(scope="class")
     def fingerprint(self):
-        return prolif.Fingerprint("all")
+        return Fingerprint()
 
-    def test_ionic(self, fingerprint, cation, anion):
-        assert fingerprint.anionic(anion, cation) is True
-        assert fingerprint.cationic(cation, anion) is True
-        assert fingerprint.anionic(cation, anion) is False
-        assert fingerprint.cationic(anion, cation) is False
-
-    def test_cation_pi(self, fingerprint, benzene, cation):
-        false = from_mol2("cation_false.mol2")
-        assert fingerprint.cationpi(cation, benzene) is True
-        assert fingerprint.pication(benzene, cation) is True
-        assert fingerprint.cationpi(false, benzene) is False
-        assert fingerprint.cationpi(benzene, cation) is False
-        assert fingerprint.pication(benzene, false) is False
-        assert fingerprint.pication(cation, benzene) is False
-
-    def test_pistacking(self, fingerprint, benzene, ftf, etf):
-        assert fingerprint.facetoface(benzene, ftf) is True
-        assert fingerprint.facetoface(ftf, benzene) is True
-        assert fingerprint.facetoface(benzene, etf) is False
-        assert fingerprint.facetoface(etf, benzene) is False
-        assert fingerprint.edgetoface(benzene, etf) is True
-        assert fingerprint.edgetoface(etf, benzene) is True
-        assert fingerprint.edgetoface(benzene, ftf) is False
-        assert fingerprint.edgetoface(ftf, benzene) is False
-        assert fingerprint.pistacking(benzene, etf) is True
-        assert fingerprint.pistacking(etf, benzene) is True
-        assert fingerprint.pistacking(ftf, benzene) is True
-        assert fingerprint.pistacking(benzene, ftf) is True
-
-    def test_hydrophobic(self, fingerprint, ftf, etf, anion, cation, benzene):
-        chlorine = from_mol2("chlorine.mol2")
-        assert fingerprint.hydrophobic(benzene, etf) is True
-        assert fingerprint.hydrophobic(benzene, ftf) is True
-        assert fingerprint.hydrophobic(benzene, chlorine) is True
-        assert fingerprint.hydrophobic(benzene, anion) is False
-        assert fingerprint.hydrophobic(benzene, cation) is False
-
-    def test_hbond(self, fingerprint):
-        donor = from_mol2("donor.mol2")
-        acceptor = from_mol2("acceptor.mol2")
-        acceptor_false = from_mol2("acceptor_false.mol2")
-        assert fingerprint.hbdonor(donor, acceptor) is True
-        assert fingerprint.hbacceptor(acceptor, donor) is True
-        assert fingerprint.hbacceptor(acceptor_false, donor) is False
-        assert fingerprint.hbdonor(donor, acceptor_false) is False
-        assert fingerprint.hbdonor(acceptor, donor) is False
-        assert fingerprint.hbacceptor(donor, acceptor) is False
-
-    def test_xbond(self, fingerprint):
-        donor = from_mol2("xbond_donor.mol2")
-        acceptor = from_mol2("xbond_acceptor.mol2")
-        acceptor_false_xar = from_mol2("xbond_acceptor_false_xar.mol2")
-        acceptor_false_axd = from_mol2("xbond_acceptor_false_axd.mol2")
-        assert fingerprint.xbdonor(donor, acceptor) is True
-        assert fingerprint.xbacceptor(acceptor, donor) is True
-        assert fingerprint.xbacceptor(acceptor_false_xar, donor) is False
-        assert fingerprint.xbdonor(donor, acceptor_false_xar) is False
-        assert fingerprint.xbacceptor(acceptor_false_axd, donor) is False
-        assert fingerprint.xbdonor(donor, acceptor_false_axd) is False
-        assert fingerprint.xbdonor(acceptor, donor) is False
-        assert fingerprint.xbacceptor(donor, acceptor) is False
-
-    def test_metallic(self, fingerprint):
-        ligand = from_mol2("ligand.mol2")
-        metal = from_mol2("metal.mol2")
-        metal_false = from_mol2("metal_false.mol2")
-        assert fingerprint.metaldonor(metal, ligand) is True
-        assert fingerprint.metalacceptor(ligand, metal) is True
-        assert fingerprint.metalacceptor(ligand, metal_false) is False
-        assert fingerprint.metaldonor(metal_false, ligand) is False
-        assert fingerprint.metalacceptor(metal, ligand) is False
-        assert fingerprint.metaldonor(ligand, metal) is False
+    @pytest.mark.parametrize("func_name, mol1, mol2, expected", [
+        ("cationic", "cation", "anion", True),
+        ("cationic", "anion", "cation", False),
+        ("cationic", "cation", "benzene", False),
+        ("anionic", "cation", "anion", False),
+        ("anionic", "anion", "cation", True),
+        ("anionic", "anion", "benzene", False),
+        ("cationpi", "cation", "benzene", True),
+        ("cationpi", "cation_false", "benzene", False),
+        ("cationpi", "benzene", "cation", False),
+        ("cationpi", "cation", "cation", False),
+        ("cationpi", "benzene", "benzene", False),
+        ("pication", "benzene", "cation", True),
+        ("pication", "benzene", "cation_false", False),
+        ("pication", "cation", "benzene", False),
+        ("pication", "cation", "cation", False),
+        ("pication", "benzene", "benzene", False),
+        ("pistacking", "benzene", "etf", True),
+        ("pistacking", "etf", "benzene", True),
+        ("pistacking", "ftf", "benzene", True),
+        ("pistacking", "benzene", "ftf", True),
+        ("facetoface", "benzene", "ftf", True),
+        ("facetoface", "ftf", "benzene", True),
+        ("facetoface", "benzene", "etf", False),
+        ("facetoface", "etf", "benzene", False),
+        ("edgetoface", "benzene", "etf", True),
+        ("edgetoface", "etf", "benzene", True),
+        ("edgetoface", "benzene", "ftf", False),
+        ("edgetoface", "ftf", "benzene", False),
+        ("hydrophobic", "benzene", "etf", True),
+        ("hydrophobic", "benzene", "ftf", True),
+        ("hydrophobic", "benzene", "chlorine", True),
+        ("hydrophobic", "benzene", "anion", False),
+        ("hydrophobic", "benzene", "cation", False),
+        ("hbdonor", "donor", "acceptor", True),
+        ("hbdonor", "donor", "acceptor_false", False),
+        ("hbdonor", "acceptor", "donor", False),
+        ("hbacceptor", "acceptor", "donor", True),
+        ("hbacceptor", "acceptor_false", "donor", False),
+        ("hbacceptor", "donor", "acceptor", False),
+        ("xbdonor", "donor", "acceptor", True),
+        ("xbdonor", "donor", "acceptor_false_xar", False),
+        ("xbdonor", "donor", "acceptor_false_axd", False),
+        ("xbdonor", "acceptor", "donor", False),
+        ("xbacceptor", "acceptor", "donor", True),
+        ("xbacceptor", "acceptor_false_xar", "donor", False),
+        ("xbacceptor", "acceptor_false_axd", "donor", False),
+        ("xbacceptor", "donor", "acceptor", False),
+        ("metaldonor", "metal", "ligand", True),
+        ("metaldonor", "metal_false", "ligand", False),
+        ("metaldonor", "ligand", "metal", False),
+        ("metalacceptor", "ligand", "metal", True),
+        ("metalacceptor", "ligand", "metal_false", False),
+        ("metalacceptor", "metal", "ligand", False),
+    ], indirect=["mol1", "mol2"])
+    def test_interaction(self, fingerprint, func_name, mol1, mol2, expected):
+        interaction = getattr(fingerprint, func_name)
+        assert interaction(mol1, mol2) is expected
