348802bf9b7c66b670103bb6514718d8ffcc1e8d
Cédric Bouysset <cedric.bouysset@unice.fr>
2020-12-10 01:51:12 +0100

    fix IsADirectoryError on py3.9

--- prolif/datafiles.py
+++ prolif/datafiles.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import importlib.resources
 
-with importlib.resources.path("data", "") as p:
-    datapath = p
+with importlib.resources.path("data", "top.pdb") as p:
+    datapath = p.parent
 
 TOP = str(datapath / "top.pdb")
 TRAJ = str(datapath / "traj.xtc")
