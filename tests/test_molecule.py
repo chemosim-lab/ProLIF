26a7e6e22d700b4aee32d92cad571b14c5c0a06b
Cédric Bouysset <cedric.bouysset@unice.fr>
2021-02-02 21:09:39 +0100

    add tests

--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -1,10 +1,11 @@
 import pytest
+from rdkit import Chem
 from prolif.molecule import (Molecule,
                              pdbqt_supplier,
                              mol2_supplier,
-                             sdf_supplier,
-                             _patch_rdkit_mol)
+                             sdf_supplier)
 from prolif.residue import ResidueId
+from prolif.datafiles import datapath
 from .test_base import TestBaseRDKitMol, rdkit_mol, ligand_rdkit, u
 
 
@@ -23,6 +24,17 @@ class TestMolecule(TestBaseRDKitMol):
         assert rdkit_mol[0].resid == mda_mol[0].resid
         assert (rdkit_mol.HasSubstructMatch(mda_mol) and
                 mda_mol.HasSubstructMatch(rdkit_mol))
+    
+    def test_from_rdkit(self):
+        rdkit_mol = Molecule(ligand_rdkit)
+        newmol = Molecule.from_rdkit(ligand_rdkit)
+        assert rdkit_mol[0].resid == newmol[0].resid
+        mol = Chem.MolFromSmiles("CCO")
+        newmol = Molecule.from_rdkit(mol)
+        assert newmol[0].resid == ResidueId("UNL", 1)
+        newmol = Molecule.from_rdkit(mol, "FOO", 42, "A")
+        assert newmol[0].resid == ResidueId("FOO", 42, "A")
+
 
     @pytest.mark.parametrize("key", [
         0,
@@ -40,3 +52,38 @@ class TestMolecule(TestBaseRDKitMol):
 
     def test_n_residues(self, mol):
         assert mol.n_residues == mol.residues.n_residues
+
+
+class TestSupplier:
+    def test_pdbqt(self):
+        path = datapath / "vina"
+        pdbqts = sorted(path.glob("*.pdbqt"))
+        template = Chem.MolFromSmiles("C[NH+]1CC(C(=O)NC2(C)OC3(O)C4CCCN4C(=O)"
+                                      "C(Cc4ccccc4)N3C2=O)C=C2c3cccc4[nH]cc"
+                                      "(c34)CC21")
+        suppl = pdbqt_supplier(pdbqts, template)
+        mols = list(suppl)
+        assert isinstance(mols[0], Molecule)
+        assert len(mols) == len(pdbqts)
+
+    def test_sdf(self):
+        path = str(datapath / "vina" / "vina_output.sdf")
+        suppl = sdf_supplier(path)
+        mols = list(suppl)
+        assert isinstance(mols[0], Molecule)
+        assert len(mols) == 9
+        mi = mols[0].GetAtomWithIdx(0).GetMonomerInfo()
+        assert all([mi.GetResidueName() == "UNL",
+                    mi.GetResidueNumber() == 1,
+                    mi.GetChainId() == ""])
+
+    def test_mol2(self):
+        path = str(datapath / "vina" / "vina_output.mol2")
+        suppl = mol2_supplier(path)
+        mols = list(suppl)
+        assert isinstance(mols[0], Molecule)
+        assert len(mols) == 9
+        mi = mols[0].GetAtomWithIdx(0).GetMonomerInfo()
+        assert all([mi.GetResidueName() == "UNL",
+                    mi.GetResidueNumber() == 1,
+                    mi.GetChainId() == ""])
