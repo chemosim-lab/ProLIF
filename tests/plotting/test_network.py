5722e305b54a111e8e0593c168cbdbba64321f21
Cédric Bouysset <bouysset.cedric@gmail.com>
2021-06-08 18:03:38 +0200

    fix lignetwork

--- tests/plotting/test_network.py
+++ tests/plotting/test_network.py
@@ -3,19 +3,17 @@ import prolif as plf
 from prolif.plotting.network import LigNetwork
 import pytest
 
-def test_from_ifp_raises_kind():
-    with pytest.raises(ValueError, match='must be "aggregate" or "frame"'):
-        LigNetwork.from_ifp(None, kind="foo")
 
 @pytest.fixture(scope="module")
 def lignetwork_data():
     u = mda.Universe(plf.datafiles.TOP)
-    prot = u.select_atoms("protein")
+    prot = u.select_atoms("protein and resid 120-130")
     lig = u.select_atoms("resname LIG")
-    fp = plf.Fingerprint(["HBDonor"])
-    fp.run(u.trajectory, lig, prot, return_atoms=True)
-    df = fp.to_dataframe()
+    fp = plf.Fingerprint(["Hydrophobic"])
+    fp.run(u.trajectory, lig, prot)
+    df = fp.to_dataframe(return_atoms=True)
     return lig, df
+
 
 def test_integration_frame(lignetwork_data):
     lig, df = lignetwork_data
@@ -23,8 +21,15 @@ def test_integration_frame(lignetwork_data):
     html = net._get_html()
     assert html
 
+
 def test_integration_agg(lignetwork_data):
     lig, df = lignetwork_data
     net = LigNetwork.from_ifp(df, lig, kind="aggregate", threshold=0)
     html = net._get_html()
     assert html
+
+
+def test_from_ifp_raises_kind(lignetwork_data):
+    lig, df = lignetwork_data
+    with pytest.raises(ValueError, match='must be "aggregate" or "frame"'):
+        LigNetwork.from_ifp(df, lig, kind="foo")
