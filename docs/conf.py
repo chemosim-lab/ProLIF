887f98f2d3dd2c8893706ac57d45fe6e69d2b046
Cédric Bouysset <bouysset.cedric@gmail.com>
2021-08-18 15:08:52 +0200

    fix RTD notebook rendering

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-source_suffix = ['.rst', '.md', '.ipynb']
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 
