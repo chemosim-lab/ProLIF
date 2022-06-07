708d84ee5ba0d8fdd00694749ebc331021bb3e2d
Cédric Bouysset <bouysset.cedric@gmail.com>
2022-06-07 22:23:38 +0100

    fix setup script for build

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 import versioneer
 import re
+import os
+
+GITHUB_ACTIONS = os.environ.get("GITHUB_ACTION", False)
 
 # manually check RDKit version
 try:
     from rdkit import __version__ as rdkit_version
 except ImportError:
-    raise ImportError("ProLIF requires RDKit but it is not installed")
+    if not GITHUB_ACTIONS:
+        raise ImportError("ProLIF requires RDKit but it is not installed")
 else:
     if re.match(r"^20[0-1][0-9]\.", rdkit_version):
         raise ValueError("ProLIF requires a version of RDKit >= 2020")
