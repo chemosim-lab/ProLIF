1590f4ed1e3d4cbe0b7c42bbfa49917295c00fb2
Cédric Bouysset <bouysset.cedric@gmail.com>
2023-04-06 17:22:47 +0200

    switch to pyproject

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-import os
-import re
-
 from setuptools import setup
-
-GITHUB_ACTIONS = os.environ.get("GITHUB_ACTIONS", False)
-READTHEDOCS = os.environ.get("READTHEDOCS", False)
-
-# manually check RDKit version
-try:
-    from rdkit import __version__ as rdkit_version
-except ImportError:
-    if not (GITHUB_ACTIONS or READTHEDOCS):
-        raise ImportError("ProLIF requires RDKit but it is not installed")
-else:
-    if re.match(r"^(20[0-1][0-9])|(2020)", rdkit_version):
-        raise ValueError("ProLIF requires a version of RDKit >= 2021")
 
 setup()
