2e3ed0a7f46e99bd99996e583f54fd44132e7372
Cédric Bouysset <bouysset.cedric@gmail.com>
2023-08-31 23:09:03 +0100

    add residue display function

--- prolif/__init__.py
+++ prolif/__init__.py
@@ -2,5 +2,6 @@ from prolif import datafiles
 from prolif._version import __version__
 from prolif.fingerprint import Fingerprint
 from prolif.molecule import Molecule, mol2_supplier, pdbqt_supplier, sdf_supplier
+from prolif.plotting.residues import display_residues
 from prolif.residue import ResidueId
 from prolif.utils import get_residues_near_ligand, to_bitvectors, to_dataframe
