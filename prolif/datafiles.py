9c44357ddc411bd038928df2866dccc1dd60c920
Cédric Bouysset <bouysset.cedric@gmail.com>
2024-06-22 00:41:47 +0100

    switch to importlib.resources

--- prolif/datafiles.py
+++ prolif/datafiles.py
@@ -1,8 +1,11 @@
-from pathlib import Path
+import atexit
+from contextlib import ExitStack
+from importlib import resources
 
-from pkg_resources import resource_filename
-
-datapath = Path(resource_filename("prolif", "data/"))
+_file_manager = ExitStack()
+atexit.register(_file_manager.close)
+_data_resource = resources.files("prolif") / "data/"
+datapath = _file_manager.enter_context(resources.as_file(_data_resource))
 
 TOP = str(datapath / "top.pdb")
 TRAJ = str(datapath / "traj.xtc")
