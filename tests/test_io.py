8d67d5d803e790d6ff58376ffda061ba86a4309d
Yu-Yuan (Stuart) Yang - from connectin, AFLab <yu-yuan.yang@qmul.ac.uk>
2025-06-24 11:27:50 +0100

    add more tests for protein helper

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,10 +2,12 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
+from numpy.testing import assert_equal
 from rdkit import Chem
 
 from prolif.datafiles import datapath
 from prolif.molecule import Molecule
+from prolif.residue import Residue
 
 
 @pytest.fixture(scope="module")
@@ -74,10 +76,28 @@ class TestProteinHelper:
         return str(datapath / "tpo.pdb")
 
     @pytest.fixture(scope="class")
-    def INPUT_MOL(self, INPUT_PATH) -> Molecule:
+    def INPUT_MOL(self, INPUT_PATH: str) -> Molecule:
         """Return the Molecule object for the input file."""
         input_mol = Chem.MolFromPDBFile(INPUT_PATH)
         return Molecule.from_rdkit(input_mol)
+
+    @pytest.fixture(scope="class")
+    def CUSTOM_TEMPLATE(self) -> dict:
+        """Return a custom template for testing."""
+        from prolif.io.cif import cif_template_reader
+
+        tpo_template = cif_template_reader(datapath / "TPO.cif")
+        return {
+            "XYZ": {
+                "name": "XYZ",
+                "SMILES": "C1=CC=CC=C1",
+            },
+            "ABC": {
+                "name": "ABC",
+                "SMILES": "C(C(=O)O)N",
+            },
+            "TPO": tpo_template["TPO"],
+        }
 
     def test_initialization(self, INPUT_PATH: str, INPUT_MOL: Molecule) -> None:
         """Test the initialization of the TestProteinHelper class."""
@@ -96,3 +116,145 @@ class TestProteinHelper:
         # test type error
         with pytest.raises(TypeError):
             ProteinHelper(12345)
+
+    def test_convert_to_standard_resname(self) -> None:
+        """Test the conversion of residue names to standard names."""
+        from prolif.io.protein_helper import ProteinHelper
+
+        # warning with unknown forcefield name
+        with pytest.warns(
+            UserWarning,
+            match=r"Could not guess the forcefield based on the residue names\. "
+            r"CYS is assigned to neutral CYS \(charge = 0\)\.",
+        ):
+            ProteinHelper.convert_to_standard_resname(
+                resname="CYS", forcefield_name="unknown"
+            )
+
+        # gromos's CYS -> CYX
+        resname = ProteinHelper.convert_to_standard_resname(
+            resname="CYS", forcefield_name="gromos"
+        )
+        assert resname == "CYX"
+
+        # Test with an unknown residue name
+        resname = ProteinHelper.convert_to_standard_resname(
+            resname="HSD", forcefield_name="unknown"
+        )
+        assert resname == "HID"
+
+    def test_check_resnames(self, CUSTOM_TEMPLATE: dict) -> None:
+        """Test the checking of residue names."""
+        from prolif.io.protein_helper import ProteinHelper
+
+        # Test with resnames not within a default templates
+        with pytest.raises(
+            ValueError,
+            match=r"Residue \{'XYZ'\} is not a standard residue or "
+            r"not in the templates\. Please provide a custom template\.",
+        ):
+            ProteinHelper.check_resnames({"ALA", "CYS", "XYZ"})
+
+        # Test with resnames not with custom template
+        with pytest.raises(
+            ValueError,
+            match=r"Residue \{'ALA'\} is not a standard residue or "
+            r"not in the templates\. Please provide a custom template\.",
+        ):
+            ProteinHelper.check_resnames(
+                {"ALA", "ABC", "XYZ"}, templates=[CUSTOM_TEMPLATE]
+            )
+
+    def test_n_residue_heavy_atoms(self, INPUT_MOL: Molecule) -> None:
+        """Test the counting of heavy atoms in residues."""
+        from prolif.io.protein_helper import ProteinHelper
+
+        # Test with a Molecule object
+        n_heavy_atoms = ProteinHelper.n_residue_heavy_atoms(INPUT_MOL.residues[1])
+        assert isinstance(n_heavy_atoms, int)
+        assert n_heavy_atoms == 11
+
+    def test_n_template_residue_heavy_atoms(self, CUSTOM_TEMPLATE: dict) -> None:
+        """Test the counting of heavy atoms in template residues."""
+        from prolif.io.protein_helper import ProteinHelper
+
+        # Test with a standard amino acid template
+        template_n_heavy_atoms = ProteinHelper.n_template_residue_heavy_atoms()
+        assert isinstance(template_n_heavy_atoms, dict)
+        assert template_n_heavy_atoms["ALA"] == 5
+
+        # Test with a custom (SMILES) template
+        custom_template_n_heavy_atoms = ProteinHelper.n_template_residue_heavy_atoms(
+            templates=[CUSTOM_TEMPLATE]
+        )
+        assert isinstance(custom_template_n_heavy_atoms, dict)
+        assert custom_template_n_heavy_atoms["XYZ"] == 6
+
+    def test_fix_molecule_bond_orders(
+        self, INPUT_MOL: Molecule, CUSTOM_TEMPLATE: dict
+    ) -> None:
+        """Test the fixing of bond orders in a Molecule object."""
+        from prolif.io.protein_helper import ProteinHelper
+
+        # Test with a Molecule object (using CIF template)
+        fixed_mol = ProteinHelper.fix_molecule_bond_orders(
+            INPUT_MOL.residues[1], templates=[CUSTOM_TEMPLATE]
+        )
+        assert isinstance(fixed_mol, Residue)
+        # assert len(fixed_mol.bonds) > 0
+
+        # Test with a Molecule object (using SMILES template)
+        fixed_mol_custom = ProteinHelper.fix_molecule_bond_orders(
+            INPUT_MOL.residues[1],
+            templates=[
+                {
+                    "TPO": {
+                        "name": "TPO",
+                        "SMILES": "C[C@H]([C@@H](C(=O))N)OP(=O)(O)O",
+                    }
+                }
+            ],
+        )
+        assert isinstance(fixed_mol_custom, Residue)
+
+        # check two residue are equal and bond orders are fixed
+        for at1, at2 in zip(
+            fixed_mol.GetAtoms(), fixed_mol_custom.GetAtoms(), strict=False
+        ):
+            assert at1.GetSymbol() == at2.GetSymbol()
+
+        bond1_info, bond2_info = [], []
+        for bond in fixed_mol.GetBonds():
+            if bond.GetBeginAtomIdx() < bond.GetEndAtomIdx():
+                bond1_info.append(
+                    f"{bond.GetBeginAtomIdx()}_{bond.GetEndAtomIdx()}_"
+                    f"{int(bond.GetBondTypeAsDouble())}"
+                )
+            else:
+                bond1_info.append(
+                    f"{bond.GetEndAtomIdx()}_{bond.GetBeginAtomIdx()}_"
+                    f"{int(bond.GetBondTypeAsDouble())}"
+                )
+
+        for bond in fixed_mol_custom.GetBonds():
+            if bond.GetBeginAtomIdx() < bond.GetEndAtomIdx():
+                bond2_info.append(
+                    f"{bond.GetBeginAtomIdx()}_{bond.GetEndAtomIdx()}_"
+                    f"{int(bond.GetBondTypeAsDouble())}"
+                )
+            else:
+                bond2_info.append(
+                    f"{bond.GetEndAtomIdx()}_{bond.GetBeginAtomIdx()}_"
+                    f"{int(bond.GetBondTypeAsDouble())}"
+                )
+        bond1_info = sorted(bond1_info)
+        bond2_info = sorted(bond2_info)
+        assert_equal(bond1_info, bond2_info)
+
+        with pytest.raises(
+            ValueError,
+            match=r"Failed to find template for residue: \'ACE\'",
+        ):
+            ProteinHelper.fix_molecule_bond_orders(
+                INPUT_MOL.residues[0], templates=CUSTOM_TEMPLATE
+            )
