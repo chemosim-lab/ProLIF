dfe78c304b9f2e455d624eb45f46d29d0f09c6b6
Yu-Yuan (Stuart) Yang - from connectin, AFLab <yu-yuan.yang@qmul.ac.uk>
2025-07-03 17:35:48 +0100

    update test_io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pandas as pd
 import pytest
 from numpy.testing import assert_equal
@@ -7,6 +5,13 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 
 from prolif.datafiles import datapath
+from prolif.io.cif import cif_parser_lite, cif_template_reader
+from prolif.io.protein_helper import (
+    ProteinHelper,
+    _assign_intra_props_lone_H,
+    strip_bonds,
+)
+from prolif.io.xml import parse_altnames
 from prolif.molecule import Molecule
 from prolif.residue import Residue
 
@@ -14,18 +19,17 @@ from prolif.residue import Residue
 @pytest.fixture(scope="module")
 def CIF() -> str:
     """Fixture to load the CIF file for testing."""
-    return Path(str(datapath / "TPO.cif")).read_text()
+    return (datapath / "TPO.cif").read_text()
 
 
 @pytest.fixture(scope="module")
 def STANDARD_AA() -> str:
     """Fixture to load the standard amino acid CIF file for testing."""
-    return Path(str(datapath / "standard_aa.cif")).read_text()
+    return (datapath / "standard_aa.cif").read_text()
 
 
 def test_cif_parser_lite(CIF: str, STANDARD_AA: str) -> None:
-    from prolif.io.cif import cif_parser_lite
-
+    """Test the CIF parser for a valid CIF file and a standard amino acid CIF file."""
     # Test with a valid CIF file
     result = cif_parser_lite(CIF)
 
@@ -42,7 +46,6 @@ def test_cif_parser_lite(CIF: str, STANDARD_AA: str) -> None:
 
 def test_cif_template_reader() -> None:
     """Test the CIF template reader."""
-    from prolif.io.cif import cif_template_reader
 
     # Test reading the standard amino acid CIF template
     result = cif_template_reader(datapath / "TPO.cif")
@@ -56,12 +59,11 @@ def test_cif_template_reader() -> None:
 @pytest.fixture(scope="module")
 def XML_TEST_DATA() -> str:
     """Fixture to load the XML test data for altnames parsing."""
-    return Path(str(datapath / "standard_aa_name.xml")).read_text()
+    return (datapath / "standard_aa_name.xml").read_text()
 
 
 def test_xml_parse_altnames(XML_TEST_DATA: str) -> None:
     """Test the XML parsing of alternative names for residues and atoms."""
-    from prolif.io.xml import parse_altnames
 
     resname_aliases, atomname_aliases = parse_altnames(XML_TEST_DATA)
     assert isinstance(resname_aliases, dict)
@@ -92,7 +94,6 @@ class TestProteinHelper:
     @pytest.fixture(scope="class")
     def CUSTOM_TEMPLATE(self) -> dict:
         """Return a custom template for testing."""
-        from prolif.io.cif import cif_template_reader
 
         tpo_template = cif_template_reader(datapath / "TPO.cif")
         ace_template = cif_template_reader(datapath / "ACE.cif")
@@ -123,7 +124,6 @@ class TestProteinHelper:
 
     def test_initialization(self, INPUT_PATH: str, INPUT_MOL: Molecule) -> None:
         """Test the initialization of the TestProteinHelper class."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # test reading with a path
         protein_helper = ProteinHelper(INPUT_PATH)
@@ -141,7 +141,6 @@ class TestProteinHelper:
 
     def test_convert_to_standard_resname(self) -> None:
         """Test the conversion of residue names to standard names."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # warning with unknown forcefield name
         with pytest.warns(
@@ -167,7 +166,6 @@ class TestProteinHelper:
 
     def test_check_resnames(self, CUSTOM_TEMPLATE: dict) -> None:
         """Test the checking of residue names."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # Test with resnames not within a default templates
         with pytest.raises(
@@ -189,7 +187,6 @@ class TestProteinHelper:
 
     def test_n_residue_heavy_atoms(self, INPUT_MOL: Molecule) -> None:
         """Test the counting of heavy atoms in residues."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # Test with a Molecule object
         n_heavy_atoms = ProteinHelper.n_residue_heavy_atoms(INPUT_MOL.residues[1])
@@ -198,7 +195,6 @@ class TestProteinHelper:
 
     def test_n_template_residue_heavy_atoms(self, CUSTOM_TEMPLATE: dict) -> None:
         """Test the counting of heavy atoms in template residues."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # Test with a standard amino acid template
         template_n_heavy_atoms = ProteinHelper.n_template_residue_heavy_atoms()
@@ -219,7 +215,6 @@ class TestProteinHelper:
         self, INPUT_MOL: Molecule, CUSTOM_TEMPLATE: dict
     ) -> None:
         """Test the fixing of bond orders in a Molecule object."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # Test with a Molecule object (using CIF template)
         fixed_mol = ProteinHelper.fix_molecule_bond_orders(
@@ -247,33 +242,11 @@ class TestProteinHelper:
         ):
             assert at1.GetSymbol() == at2.GetSymbol()
 
-        bond1_info, bond2_info = [], []
-        for bond in fixed_mol.GetBonds():
-            if bond.GetBeginAtomIdx() < bond.GetEndAtomIdx():
-                bond1_info.append(
-                    f"{bond.GetBeginAtomIdx()}_{bond.GetEndAtomIdx()}_"
-                    f"{int(bond.GetBondTypeAsDouble())}"
-                )
-            else:
-                bond1_info.append(
-                    f"{bond.GetEndAtomIdx()}_{bond.GetBeginAtomIdx()}_"
-                    f"{int(bond.GetBondTypeAsDouble())}"
-                )
-
-        for bond in fixed_mol_custom.GetBonds():
-            if bond.GetBeginAtomIdx() < bond.GetEndAtomIdx():
-                bond2_info.append(
-                    f"{bond.GetBeginAtomIdx()}_{bond.GetEndAtomIdx()}_"
-                    f"{int(bond.GetBondTypeAsDouble())}"
-                )
-            else:
-                bond2_info.append(
-                    f"{bond.GetEndAtomIdx()}_{bond.GetBeginAtomIdx()}_"
-                    f"{int(bond.GetBondTypeAsDouble())}"
-                )
-        bond1_info = sorted(bond1_info)
-        bond2_info = sorted(bond2_info)
-        assert_equal(bond1_info, bond2_info)
+        for bond1 in fixed_mol.GetBonds():
+            bond2 = fixed_mol_custom.GetBondBetweenAtoms(
+                bond1.GetBeginAtomIdx(), bond1.GetEndAtomIdx()
+            )
+            assert bond1.GetBondType() == bond2.GetBondType()
 
         # Test: not found template for residue (TPO) in default templates
         with pytest.raises(
@@ -282,25 +255,22 @@ class TestProteinHelper:
         ):
             ProteinHelper.fix_molecule_bond_orders(INPUT_MOL.residues[1])
 
-    def test_forcefield_guesser(self) -> None:
+    @pytest.mark.parametrize(
+            ("resnames", "expected"),
+            [
+                ({"HSD"}, "charmm"),
+                ({"NASP"}, "amber"),
+                ({"ASN1"}, "gromos"),
+                ({"HISD"}, "oplsaa"),
+                ({"CYS"}, "unknown"),
+            ]
+    )
+    def test_forcefield_guesser(self, resnames, expected) -> None:
         """Test the forcefield guesser."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # Test with a known forcefield
-        forcefield = ProteinHelper.forcefield_guesser({"HSD"})
-        assert forcefield == "charmm"
-
-        forcefield = ProteinHelper.forcefield_guesser({"NASP"})
-        assert forcefield == "amber"
-
-        forcefield = ProteinHelper.forcefield_guesser({"ASN1"})
-        assert forcefield == "gromos"
-
-        forcefield = ProteinHelper.forcefield_guesser({"HISD"})
-        assert forcefield == "oplsaa"
-
-        forcefield = ProteinHelper.forcefield_guesser({"CYS"})
-        assert forcefield == "unknown"
+        forcefield = ProteinHelper.forcefield_guesser(resnames)
+        assert forcefield == expected
 
     def test_standardize_protein(
         self,
@@ -310,7 +280,6 @@ class TestProteinHelper:
         HSD_RESIDUE: Residue,
     ) -> None:
         """Test the standardization of a protein molecule."""
-        from prolif.io.protein_helper import ProteinHelper
 
         # Test standardizing a protein molecule
         protein_helper = ProteinHelper(INPUT_MOL)
@@ -406,7 +375,6 @@ class TestProteinHelper:
         """
         Test the assignment of intra properties for a residue with a lone hydrogen.
         """
-        from prolif.io.protein_helper import _assign_intra_props_lone_H, strip_bonds
 
         mol = Chem.MolFromSmiles("CC")
         mol = Chem.AddHs(mol)
