54ff621c94065d370ecd2a6149a381bdd5a14607
Cédric Bouysset <bouysset.cedric@gmail.com>
2025-10-30 02:08:40 +0000

    fix: __version__ metadata

--- prolif/_version.py
+++ prolif/_version.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 
 try:
-    __version__ = importlib.metadata.version(__name__)
+    __version__ = importlib.metadata.version("prolif")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0"
